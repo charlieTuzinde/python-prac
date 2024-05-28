160969dca17fd168d170befd857d455066c789f3
charlieTuzinde <130315633+charlieTuzinde@users.noreply.github.com>
2024-05-28 10:50:17 +0300

    Add files via upload

---
+++ python prac/program examples/1.py
@@ -0,0 +1,5 @@
+# The following program prints Tuzinde a random number of times between 5 and 25.
+from random import randint 
+rand_num = randint (5,25)
+for i in range (rand_num):
+    print ("Tuzinde")
---
+++ python prac/program examples/2.py
@@ -0,0 +1,5 @@
+from random import randint 
+
+rand_num = randint (1,5)
+for i in range(6):
+    print('Tuzinde '*rand_num)
